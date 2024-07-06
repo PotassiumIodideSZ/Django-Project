6eb2f79fd106705a7ddb24e9417ad46d85141e58
PotassiumIodideSZ <skobelkin.zahar@gmail.com>
2024-07-07 01:45:11 +0500

    1.3

--- myproject/flights/views.py
+++ myproject/flights/views.py
@@ -3,13 +3,31 @@ from django.http import JsonResponse
 from django.views import View
 import os
 
+def get_iata_code(city_name_from, city_name_to):
+    url = f"https://www.travelpayouts.com/widgets_suggest_params?q=Из%20{city_name_from}%20в%20{city_name_to}"
+    response = requests.get(url)
+    try:
+        data = response.json()
+        return data
+    except ValueError:
+        return None
+
 class FlightSearchView(View):
     def get(self, request):
+        origin = request.GET.get('origin')
+        destination = request.GET.get('destination')
+
+        # Преобразование названий городов в IATA-коды
+        codes = get_iata_code(origin, destination)
+
+        if not codes['origin']['iata'] or not codes['destination']['iata']:
+            return JsonResponse({'error': 'Invalid origin or destination city'}, status=400)
+
         url = "https://api.travelpayouts.com/aviasales/v3/prices_for_dates"
         params = {
             'currency': request.GET.get('currency', 'rub'),
-            'origin': request.GET.get('origin'),
-            'destination': request.GET.get('destination'),
+            'origin': codes['origin']['iata'],
+            'destination': codes['destination']['iata'],
             'departure_at': request.GET.get('departure_at'),
             'return_at': request.GET.get('return_at'),
             'one_way': request.GET.get('one_way', 'true'),
